14c4773094c2e21e0b4662da39584dd4467c8feb
Laura Guacaneme <lguacaneme@unal.edu.co>
2023-10-02 09:18:14 -0500

    Usuarios y facturas

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,95 @@
+import axios from "axios";
+
+const shipmentUrl = "http://localhost:3003/billing-api/";
+const userUrl = "http://localhost:3001/auth-api/user/"
+const companyUrl = "http://localhost:3001/auth-api/company/"
+
+
 export const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    // Users
+    userByDocument: async (root, args) => {
+      const { document } = args;
+      const user = await axios.get(userUrl + "/get/" + document).then((res) => res.data.data);
+      return user;
+    },
+
+    companyByUrl: async (root, args) => {
+      const { document } = args;
+      const shipments = await axios.get(companyUrl + "/get/" + document).then((res) => res.data.data);
+      return shipments;
+    },
+
+    // Shipments
+    allShipments: async () => {
+      const shipments = await axios.get(shipmentUrl).then((res) => res.data.data);
+      return shipments;
+    },
+
+    shipmentsById: async (root, args) => {
+      const { shipmentId } = args;
+      const shipment = await axios.get(shipmentUrl+'get-shipment/' + shipmentId).then((res) => res.data.data);
+      return shipment;
+    },
+    
+    shipmentsByUser: async (root, args) => {
+      const { user } = args;
+      const shipments = await axios.get(shipmentUrl+ 'userShipments/' + user).then((res) => res.data.data);
+      return shipments;
+    },
   },
+
+  Mutation: {
+    // Users
+    deleteUser: async (root, args) => {
+      const { document } = { args };
+      const response = await axios.delete(userUrl + "/delete" + document).then((res) => res.data.data);
+      return response;
+    },
+    signUpUser: async (root, args) => {
+      const { user } = { ...args };
+      const response = await axios.post(userUrl + "/signin", user).then((res) => res.data.data);
+      return response;
+    },
+    signUpUser: async (root, args) => {
+      const { user } = { ...args };
+      const response = await axios.post(userUrl + "/signup", user).then((res) => res.data.data);
+      return response;
+    },
+    updateUser: async (root, args) => {
+      const { document } = { args };
+      const response = await axios.put(userUrl + "/update" + document).then((res) => res.data.data);
+      return response;
+    },
+    signInCompany: async (root, args) => {
+      const { company } = { ...args };
+      const response = await axios.post(companyUrl + "/signin", company).then((res) => res.data.data);
+      return response;
+    },
+    signUpCompany: async (root, args) => {
+      const { company } = { ...args };
+      const response = await axios.post(companyUrl + "/signup", company).then((res) => res.data.data);
+      return response;
+    },
+
+    // Shipments
+    addShipment: async (root, args) => { 
+      const shipment = { ...args };
+      const response = await axios.post(shipmentUrl, shipment).then((res) => res.data.data);
+      return response;
+    },
+    updateShipment: async (root, args) => {
+      const id  =  args.id ;
+      const shipment = { ...args };
+      const response = await axios.patch(shipmentUrl + "/update/" + id, shipment).then((res) => res.data.data);
+      return response;
+    },
+    deleteShipment: async (root, args) => {
+      const id  =  args.id ;
+      const response = await axios.delete(shipmentUrl + "/" + id).then((res) => res.data.data);
+      return response;
+    }
+  }
+
+
 };
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,7 +1,85 @@
 import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-  type Query {
-    hello: String
+  type User {
+    _id: ID
+    userName: String
+    userLastname: String
+    password: String
+    document: Int
+    balance: Float
   }
+
+  type Company {
+    _id: ID
+    companyName: String
+    password: String
+    NIT: Int
+    balance: Float
+  }
+
+  type Shipment {
+    _id: ID
+    userId: String
+    companyId: String
+    shipmentValue: Int
+    shipmentDate: String
+  }
+
+  type Query {
+    userByDocument(document : Int): [User]!
+    companyByUrl(document : Int): [Company]!
+
+    allShipments: [Shipment]!
+    shipmentsById(shipmentId : String): Shipment!
+    shipmentsByUser(user : String): [Shipment]!
+  }
+
+  type Mutation {
+    deleteUser(document : Int): User
+    signInUser(
+      user_name: String
+      document: Int
+      password: String
+    ): String
+    signUpUser(
+      user_name: String
+      user_lastname: String
+      document: Int
+      balance: Float
+      password: String
+      enable: Boolean
+    ): String
+    updateUser(document: Int): [User]
+
+    signInCompany(
+      company_name : String
+      document : Int
+      password : String
+    ): String
+    signUpCompany(
+      company_name: String
+      document: Int
+      balance: Float
+      password: String
+    ): String
+
+    addShipment(
+      userId: String
+      companyId: String
+      shipmentValue: Float
+      shipmentDate: String
+    ): Shipment
+  
+    updateShipment(id: String
+      userId: String
+      companyId: String
+      shipmentValue: Float
+      shipmentDate: String
+    ): Shipment
+
+    deleteShipment(id: String): Shipment
+
+  }
+
 `;
