62260f5bdadf2fc540d0e76fd60aef1fc3c5ffc3
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-10-02 22:09:37 -0500

    Docker compose

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,55 @@
+import axios from "axios";
+
+const urlKycMs = "http://localhost:3000/kyc-api";
+const urlTransactionsMs = "http://localhost:3002/transactions-api";
+
 export const resolvers = {
   Query: {
     hello: () => "Hello world!",
+    listTransactions: async () => {
+      try {
+        const response = await axios.get(urlTransactionsMs + "/");
+        return response.data;
+      } catch (error) {
+        console.error("An error occurred");
+      }
+    },
+    getTransactionsByUserId: async (_, { id }) => {
+      try {
+        const response = await axios.get(urlTransactionsMs + "/list/" + id);
+        return response.data;
+      } catch (error) {
+        console.error("An error occurred");
+      }
+    },
+    getTransactionById: async (_, { id }) => {
+      try {
+        const response = await axios.get(urlTransactionsMs + "/detail/" + id);
+        return response.data;
+      } catch (error) {
+        console.error("An error occurred");
+      }
+    },
+  },
+  Mutation: {
+    createTransaction: async (_, { input }) => {
+      try {
+        const response = await axios.post(urlTransactionsMs + "/p2p", input);
+        return response.data;
+      } catch (error) {
+        console.error("An error occurred");
+      }
+    },
+    createChargeOrder: async (_, { input }) => {
+      try {
+        const response = await axios.post(
+          urlTransactionsMs + "/create-order/",
+          input
+        );
+        return response.data.order;
+      } catch (error) {
+        console.error("An error occurred");
+      }
+    },
   },
 };
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -3,5 +3,51 @@ import { gql } from "graphql-tag";
 export const typeDefs = gql`
   type Query {
     hello: String
+    # Transactions
+    listTransactions: [Transaction]
+    getTransactionsByUserId(id: ID!): [Transaction]
+    getTransactionById(id: ID!): Transaction
   }
+
+  type Mutation {
+    # Transactions
+    createTransaction(input: TransactionInput): Transaction
+    createChargeOrder(input: ChargeOrderInput): Order
+  }
+
+  # ----------- Transactions -----------
+  type Order {
+    id: ID
+    status: String
+    links: [OrderLinks]
+  }
+
+  type OrderLinks {
+    href: String
+    rel: String
+    method: String
+  }
+
+  input TransactionInput {
+    senderId: ID!
+    receiverId: ID!
+    amount: Float!
+  }
+
+  input ChargeOrderInput {
+    userId: ID!
+    amount: Float!
+  }
+
+  type Transaction {
+    id: ID!
+    amount: Float!
+    description: String!
+    receiverId: String
+    senderId: String
+    type: String!
+    status: String!
+  }
+
+  # ----------- KYC -----------
 `;
